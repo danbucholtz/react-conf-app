db5334531bf07518dbe50d7652acc542829e5914
Josh Thomas <jthoms1@gmail.com>
2017-08-15 13:47:53 -0500

    fixed props for the web component to use wc util.

--- src/pages/AppStart.js
+++ src/pages/AppStart.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import PageOne from './PageOne';
-import { Delegate } from './delegate'
+import { Delegate } from './delegate';
+import { wc } from '../../utils/stencil';
 export default class AppStart extends Component {
 
   render() {
     return [
-      <ion-nav root={PageOne} delegate={Delegate}></ion-nav>
+      <ion-nav ref={wc({},{
+        root: PageOne
+        delegate: Delegate
+      })}></ion-nav>
     ];
   }
 }
